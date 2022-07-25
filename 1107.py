6435dbd54876070edbbea8cc56958cea55e9db04
whitw <shkim7876@naver.com>
2022-07-25 23:08:27 +0900

    1107: 243612KB 1712ms, 더 개선하고싶음

--- 1107.py
+++ 1107.py
@@ -2,15 +2,13 @@ import sys
 from collections import deque
 import time
 
-def pressable(broken, max_val):
+def pressable(broken):
     buttons = sorted([i for i in range(len(broken)) if not broken[i]])
     pressed = deque(buttons)
     while(True): #while not empty
         if not pressed:
             return 
         item = pressed.popleft()
-        if item > max_val:
-            return
         yield item
         for b in buttons:
             if item * 10 + b != item:
@@ -22,10 +20,16 @@ def solve(N, broken):
     # broken: 길이 10 배열.
     #   키 i가 망가졌다면 broken[i]=True, else False
     result = abs(N - 100)
-
-    for i in pressable(broken, max_val=1000000):
+    # result: 지금까지 관측한 것중 최소비용.
+    # abs(N-100): 채널 100에서 +또는 -만 눌러서 이동했을 떄 최소비용
+    for i in pressable(broken):
+        # 누를 수 있는 채널들을 지속적으로 가져온다.
         if abs(N - i) + len(str(i)) < result:
             result = abs(N - i) + len(str(i))
+        elif i > N and len(str(i)) + i - N > result:
+            #i는 계속해서 커지는 형태. 채널 i에서 -로 이동했을 때 비용이
+            #이미 최소보다 크다면 채널이 더 커져봤자 이득이 없음.
+            break
     return result
 
 if __name__ == '__main__':
@@ -41,4 +45,4 @@ if __name__ == '__main__':
     begin = time.time()    
     print(solve(N, broken))
     end = time.time()
-    print(end - begin)
+    #print("runtime=", end - begin, "s")
