9889f838f1430a61049459a1160386fa5059599c
whitw <shkim7876@naver.com>
2022-07-25 23:21:10 +0900

    1107.py

--- 1107.py
+++ 1107.py
@@ -2,17 +2,12 @@ import sys
 from collections import deque
 import time
 
-def pressable(broken):
-    buttons = sorted([i for i in range(len(broken)) if not broken[i]])
-    pressed = deque(buttons)
-    while(True): #while not empty
-        if not pressed:
-            return 
-        item = pressed.popleft()
-        yield item
-        for b in buttons:
-            if item * 10 + b != item:
-                pressed.append(item * 10 + b)
+def pressable(i, broken):
+    bi = set(str(i))
+    for b in bi:
+        if broken[int(b)]:
+            return False
+    return True
 
 def solve(N, broken):
     #   N: 이동하고자 하는 채널 번호
@@ -21,15 +16,17 @@ def solve(N, broken):
     #   키 i가 망가졌다면 broken[i]=True, else False
     result = abs(N - 100)
     # result: 지금까지 관측한 것중 최소비용.
-    # abs(N-100): 채널 100에서 +또는 -만 눌러서 이동했을 떄 최소비용
-    for i in pressable(broken):
-        # 누를 수 있는 채널들을 지속적으로 가져온다.
-        if abs(N - i) + len(str(i)) < result:
-            result = abs(N - i) + len(str(i))
-        elif i > N and len(str(i)) + i - N > result:
-            #i는 계속해서 커지는 형태. 채널 i에서 -로 이동했을 때 비용이
-            #이미 최소보다 크다면 채널이 더 커져봤자 이득이 없음.
-            break
+    # abs(N-100): 채널 100에서 +또는 -만 눌러서 이동했을 때 최소비용
+    for i in range(1000000):
+        #단순한 버전. 채널 i를 누를 수 있고
+        #이때의 비용이 현재까지 최소비용보다 적으면 갱신
+        if pressable(i, broken):
+            li = len(str(i))
+            if abs(N-i) + li < result:
+                result = abs(N-i) + li
+            elif li + i - N > result and i > N:
+                #i가 증가하면 반드시 비용도 증가하는 경우
+                break
     return result
 
 if __name__ == '__main__':
