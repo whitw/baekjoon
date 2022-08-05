f71678c8a336c3e8480cfae918fac78d803d449a
whitw <shkim7876@naver.com>
2022-08-05 17:22:54 +0900

    17413_short.py

--- 17413_short.py
+++ 17413_short.py
@@ -1,2 +1,2 @@
-s = input().replace('>', '<').split('<')
-print(''.join([f'<{s[i]}>' if i%2 else ' '.join([j[::-1] for j in s[i].split()]) for i in range(len(s))]))
+s=input().replace('>','<').split('<')
+print(''.join([f'<{s[i]}>'if i%2 else' '.join([j[::-1]for j in s[i].split()])for i in range(len(s))]))
