8afacffa7ac8c904d0678ebe76a6800e5d49bc42
whitw <shkim7876@naver.com>
2022-08-13 22:52:28 +0900

    removed assertions

--- 16678.py
+++ 16678.py
@@ -21,12 +21,3 @@ if __name__ == '__main__':
     for i in range(N):
         MP.append(int(read()))
     print(solve(MP))
-#    MP = [random.randint(1, 20) for _ in range(random.randint(1, 20))]
-#    print(MP)
-#    print(sorted(MP))
-#    assert(solve([1,3,5,5,6,8,9,11,12,13,13,14,14,15,15,16,18,19]) == 26)
-#    assert(solve([7,3,6,2,4])==7)
-#    assert(solve([1,2,3,4,5,6,7,8,9,10]) == 0)
-#    assert(solve([0,1,1,1,1,1,1,1,1,1,1]) == 0)
-#    assert(solve([1,2,2,2,2,2,2,2,2,2,2,2]) == 0)
-#    assert(solve([1,2,3,4,5,6,8,8,9,10]) == 1)
