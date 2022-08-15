bcb219e879def30998272b72dd86284c4bb9a16c
whitw <shkim7876@naver.com>
2022-08-15 11:40:26 +0900

    12899.py shorten & faster

--- programmers/30/12899.py
+++ programmers/30/12899.py
@@ -1,4 +1,4 @@
-def solution(n):
+def solution_old(n):
     ternary = []
     answer = ''
     while(True):
@@ -19,6 +19,15 @@ def solution(n):
                 answer += '4'
             else:
                 answer += str(i)
+    return answer
+
+def solution(n):
+    answer = ''
+    arr = ['1', '2', '4']
+    while n != 0:
+        q, r = divmod(n-1, 3)
+        answer = arr[r] + answer
+        n = q
     return answer
 
 if __name__ == '__main__':
