49bdd5cf2927ddd98be6a0bdb5090d63ee79a397
whitw <shkim7876@naver.com>
2022-08-18 00:45:00 +0900

    1939.py

---
+++ 1939.py
@@ -0,0 +1,46 @@
+from collections import deque
+import sys
+
+def solve(s, e, graph, m):
+    min_weight = 0 # 물품을 이 무게만큼 들고 있으면 반드시 배송 가능
+    max_weight = m + 1 # 물품을 이 무게만큼 들고 있으면 절대 배송 불가능
+    N = len(graph)
+    while min_weight + 1 != max_weight:
+        test_weight = (min_weight + max_weight) // 2
+        visited = [False for _ in range(N)]
+        queue = deque([s])
+        success = False
+        while queue:
+            here = queue[0]
+            queue.popleft()
+            for there, weight in graph[here]:
+                if visited[there]:
+                    continue
+                else:
+                    if weight >= test_weight:
+                        visited[there] = True
+                        if there == e:
+                            success = True
+                            break
+                        queue.append(there)
+            if success:
+                break
+        if success:
+            min_weight = test_weight
+        else:
+            max_weight = test_weight
+    return min_weight
+
+if __name__ == '__main__':
+    read = sys.stdin.readline
+    N, M = map(int, read().split())
+    graph = [[] for _ in range(N)]
+    max_weight = 0
+    for i in range(M):
+        A,B,C = map(int, read().split())
+        graph[A-1].append((B-1, C))
+        graph[B-1].append((A-1, C))
+        max_weight = max(max_weight, C)
+    s, e = map(int, read().split())
+    s, e = s-1, e-1
+    print(solve(s, e, graph, max_weight))
