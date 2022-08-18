3a15c06ff00421444609db16443c1f6cab726d38
whitw <shkim7876@naver.com>
2022-08-18 19:16:21 +0900

    1300.py

---
+++ 1300.py
@@ -0,0 +1,55 @@
+def solve(N, k):
+    ltk = 0 #이 숫자보다 작은 숫자들의 갯수가 반드시 k보다 적다
+    gtk = 10**10+1 #이 숫자보다 작은 숫자들의 갯수가 k보다 많거나 같다
+    while(ltk + 1 != gtk):
+        testk = (ltk + gtk) // 2
+        lttk = 0
+        for i in range(1, min(N, testk)+1):
+            lttk += min(N, ((testk - 1) // i))
+        if lttk < k:
+            ltk = testk
+        else:
+            gtk = testk
+        #print(f"\ttestk={testk}, lttk={lttk}, k={k}")
+    return ltk
+    #이왜맞.... 틀린것보다 더 큰일이다
+    #분석이 필요함
+
+
+def answer(N, k):
+    testarr = [0]
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            testarr.append(i*j)
+    testarr = sorted(testarr)
+    #print(testarr)
+    return testarr[k]
+
+def test(tries, N=10000):
+    import random
+    n_success=0
+    n_fail=0
+    for t in range(tries):
+        N = random.randint(1, N)
+        k = random.randint(1, N * N)
+        s = solve(N,k)
+        a = answer(N,k)
+        if s != a:
+            print(f"N, k = {N},{k}")
+            print(f"expected {a}, got {s}")
+            n_fail += 1
+            input()
+        else:
+            n_success += 1
+        if t % 10 == 0:
+            print(f"\t{t} tries")
+ 
+if __name__ == '__main__':
+    N = int(input())
+    k = int(input())
+    print(solve(N,k))
+    #print("answer:", answer(N, k))
+    #test(10000, N=10)
+    prevPrime = 2
+    maxdist = 0
+            
